d6874cbcc966cc69c745b8289e37a60e379bf60a
Matthis Krause <matthiskrause@hotmail.com>
2014-11-14 23:13:17 +0100

    Clear up initial commit. Set version number

--- core/src/de/wizard/Main.java
+++ core/src/de/wizard/Main.java
@@ -1,27 +1,32 @@
 package de.wizard;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Main extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+        private static int pauseCount = 0;
+        private static int disposeCount = 0;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+                Gdx.app.setLogLevel(Application.LOG_DEBUG);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
 	}
+
+        @Override
+        public void pause() {
+                Gdx.app.debug(this.toString(), "calling pause(): " + (++pauseCount));
+        }
+
+        @Override
+        public void dispose() {
+                Gdx.app.debug(this.toString(), "calling dispose(): " + (++disposeCount));
+        }
 }
